550fd86c93b3051a6305ba2e15551b89e26c0d0b
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-03-17 20:36:01 +0100

    operators

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtract(a, b) {
+    let difference = a - b;
+    return difference;
+}
+
+function multiply(a, b) {
+    let product = a * b;
+    return product;
+}
+
+function divide(a, b) {
+    let quotient = a / b;
+    return quotient;
+}
+
+function operate(operator, a, b) {
+    let result = operator(a, b);
+    return result;
+}
