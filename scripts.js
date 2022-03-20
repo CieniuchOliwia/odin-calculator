3f70778f5c375841eff5e235423e9e8c2286c639
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-03-20 20:12:23 +0100

    simple operations (for 2 numbers)

--- scripts.js
+++ scripts.js
@@ -1,34 +1,61 @@
 function add(a, b) {
-    let sum = a + b;
-    return sum;
-}
-
-function subtract(a, b) {
-    let difference = a - b;
-    return difference;
-}
-
-function multiply(a, b) {
-    let product = a * b;
-    return product;
-}
-
-function divide(a, b) {
-    let quotient = a / b;
-    return quotient;
-}
-
-function operate(operator, a, b) {
-    let result = operator(a, b);
+    let result = a + b;
     return result;
 }
 
+function subtract(a, b) {
+    let result = a - b;
+    return result;
+}
 
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+}
+
+function divide(a, b) {
+    let result = a / b;
+    return result;
+}
+
+function operate(operator, a, b) {
+    if (operator == 'add') {
+        return add(a, b);
+    } else if (operator == 'subtract') {
+        return subtract(a, b);
+    } else if (operator == 'multiply') {
+        return multiply(a, b);
+    } else if (operator == 'divide') {
+        return divide(a, b);
+    }
+}
+
+
+let a = '';
+let b = '';
+let operator = '';
 
 document.querySelectorAll('.button').forEach(el => {
     el.addEventListener('click', function (event) {
-        let actContent=document.getElementById('display').textContent
+        let actContent = document.getElementById('display').textContent
         let content = event.target.textContent;
-        document.getElementById('display').textContent=actContent+content;
+        if (event.target.classList.contains('number')) {
+            document.getElementById('display').textContent = '';
+            document.getElementById('display').textContent = actContent + content;
+        } else if (event.target.classList.contains('operator')) {
+            a = +document.getElementById('display').textContent;
+            operator = event.target.id;
+            //console.log('a=' + a);
+            //console.log('operator=' + operator);
+            document.getElementById('display').textContent = '';
+        } else if (event.target.classList.contains('equals')) {
+            b = +document.getElementById('display').textContent;
+            //console.log('operator=' + operator);
+            //console.log('a=' + a);
+            //console.log('b=' + b);
+            document.getElementById('display').textContent = operate(operator, a, b);
+        }
     })
 })
+
+
