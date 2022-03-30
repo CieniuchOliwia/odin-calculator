0fa08da51a2feccffcb00c84affeaf4c854750b8
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-03-30 18:56:01 +0200

    error message(divide by 0)

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 let a = '';
 let b = '';
 let operator = '';
-let display='';
-let roundedResult='';
-let indexOfComma='';
-let actContent='';
-let textOfPressedButton='';
+let display = '';
+let roundedResult = '';
+let indexOfComma = '';
+let actContent = '';
+let textOfPressedButton = '';
 
 function add(a, b) {
     return (a + b);
@@ -20,7 +20,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return (a / b);
+    if (b == 0) {
+        alert("You can't divide by 0!");
+    } else {
+        return (a / b);
+    }
 }
 
 function operate(operator, a, b) {
@@ -35,70 +39,70 @@ function operate(operator, a, b) {
     }
 }
 
-function roundResult(result){
-    if(result.length>10 && Number.isInteger(result)==false){
-        indexOfComma=result.indexOf('.');
-        return roundedResult=Math.round(result*Math.pow(10,9-indexOfComma))/(Math.pow(10,9-indexOfComma));
-    }else{
+function roundResult(result) {
+    if (result.length > 10 && Number.isInteger(result) == false) {
+        indexOfComma = result.indexOf('.');
+        return roundedResult = Math.round(result * Math.pow(10, 9 - indexOfComma)) / (Math.pow(10, 9 - indexOfComma));
+    } else {
         return result;
     }
 }
 
 
 document.querySelectorAll('.button').forEach(el => {
-    el.addEventListener('click', event=> {
-        display=document.getElementById('display');
-         actContent = display.textContent;
-         textOfPressedButton = event.target.textContent;
+    el.addEventListener('click', event => {
+        display = document.getElementById('display');
+        actContent = display.textContent;
+        textOfPressedButton = event.target.textContent;
         if (event.target.classList.contains('number')) {
-            if(operator==''){
-                a=Number(a+textOfPressedButton);
-                display.textContent = actContent+textOfPressedButton;
-            }else if (operator!=''){
-                b=Number(b+textOfPressedButton);
-                display.textContent =actContent+textOfPressedButton; 
+            if (operator == '') {
+                a = Number(a + textOfPressedButton);
+                display.textContent = actContent + textOfPressedButton;
+            } else if (operator != '') {
+                b = Number(b + textOfPressedButton);
+                display.textContent = actContent + textOfPressedButton;
             }
-            
+
         } else if (event.target.classList.contains('operator')) {
-            if(operator!=''){
-                display.textContent=roundResult((operate(operator,a,b)).toString())+textOfPressedButton;
-                a=operate(operator,a,b);
+            if (operator != '') {
+                display.textContent = roundResult((operate(operator, a, b)).toString()) + textOfPressedButton;
+                a = operate(operator, a, b);
                 operator = event.target.id;
-                b=0;
-            }else{
+                b = 0;
+            } else {
                 operator = event.target.id;
-                display.textContent = actContent+textOfPressedButton;
+                display.textContent = actContent + textOfPressedButton;
             }
-  
+
         } else if (event.target.classList.contains('equals')) {
             display.textContent = roundResult((operate(operator, a, b)).toString());
         }
     })
 })
 
-document.getElementById('AC').addEventListener('click',function(){
-    operator='';
-    display.textContent='';
-    a='';
-    b='';
+document.getElementById('AC').addEventListener('click', function () {
+    operator = '';
+    display.textContent = '';
+    a = '';
+    b = '';
 })
 
-document.addEventListener('keydown', event=> {
-
+document.addEventListener('keydown', event => {
     //console.log(parseInt(event.key));
-    display=document.getElementById('display');
+    display = document.getElementById('display');
     actContent = display.textContent;
     textOfPressedButton = event.key;
-        if (parseInt(event.key)!=='Nan') {
-            if(operator==''){
-                a=Number(a+textOfPressedButton);
-                display.textContent = actContent+textOfPressedButton;
-        }else if (operator!=''){
-           b=Number(b+textOfPressedButton);
-            display.textContent =actContent+textOfPressedButton; 
-        }}
+    if (parseInt(event.key) !== 'Nan') {
+        if (operator == '') {
+            a = Number(a + textOfPressedButton);
+            display.textContent = actContent + textOfPressedButton;
+        } else if (operator != '') {
+            b = Number(b + textOfPressedButton);
+            display.textContent = actContent + textOfPressedButton;
+        }
+    }
 })
 
-    
+
 
 
