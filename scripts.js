22678204490b9a90000d32a64c55b10c53d5b86a
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-04-02 18:51:58 +0200

    comma button

--- scripts.js
+++ scripts.js
@@ -3,9 +3,8 @@ let b = '';
 let operator = '';
 let display = '';
 let roundedResult = '';
-let indexOfComma = '';
 let actContent = '';
-let textOfPressedButton = '';
+
 
 function add(a, b) {
     return (a + b);
@@ -29,7 +28,7 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
     if (operator == 'add' || operator=='+') {
-        return add(a, b);
+        return add(Number(a), Number(b));
     } else if (operator == 'subtract' || operator=='-') {
         return subtract(a, b);
     } else if (operator == 'multiply' || operator=='*') {
@@ -40,8 +39,8 @@ function operate(operator, a, b) {
 }
 
 function roundResult(result) {
-    if (result.length > 10 && Number.isInteger(result) == false) {
-        indexOfComma = result.indexOf('.');
+    if (result.length > 10 && !Number.isInteger(result)) {
+        let indexOfComma = result.indexOf('.');
         return roundedResult = Math.round(result * Math.pow(10, 9 - indexOfComma)) / (Math.pow(10, 9 - indexOfComma));
     } else {
         return result;
@@ -53,13 +52,13 @@ document.querySelectorAll('.button').forEach(el => {
     el.addEventListener('click', event => {
         display = document.getElementById('display');
         actContent = display.textContent;
-        textOfPressedButton = event.target.textContent;
+        let textOfPressedButton = event.target.textContent;
         if (event.target.classList.contains('number')) {
             if (operator == '') {
-                a = Number(a + textOfPressedButton);
+                a =(a + textOfPressedButton);
                 display.textContent = actContent + textOfPressedButton;
             } else if (operator != '') {
-                b = Number(b + textOfPressedButton);
+                b = (b + textOfPressedButton);
                 display.textContent = actContent + textOfPressedButton;
             }
 
@@ -91,23 +90,23 @@ document.addEventListener('keydown', event => {
     //console.log(parseInt(event.key));
     display = document.getElementById('display');
     actContent = display.textContent;
-    textOfPressedButton = event.key;
-    if (isNaN(parseInt(event.key)) == false) {
-        if (operator == '') {
-            a = Number(a + textOfPressedButton);
+    let textOfPressedButton = event.key;
+    if (!isNaN(parseInt(event.key)) || event.key=='.') {
+        if (operator) { //operator nie jest pusty
+            b = (b + textOfPressedButton);
             display.textContent = actContent + textOfPressedButton;
-        } else if (operator != '') {
-            b = Number(b + textOfPressedButton);
+        } else {
+            a =(a + textOfPressedButton);
             display.textContent = actContent + textOfPressedButton;
         }
-    } else if (event.key=='+' || event.key=='-' || event.key=='*' || event.key=='/') {
-        if (operator != '') {
+    } else if (textOfPressedButton=='+' || textOfPressedButton=='-' || textOfPressedButton=='*' || textOfPressedButton=='/') {
+        if (operator) {
             display.textContent = roundResult((operate(operator, a, b)).toString()) + textOfPressedButton;
             a = operate(operator, a, b);
-            operator = event.key;
+            operator = textOfPressedButton;
             b = 0;
         } else {
-            operator = event.key;
+            operator = textOfPressedButton;
             display.textContent = actContent + textOfPressedButton;
         }
     } else {
