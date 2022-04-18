90ecda958a39e1cd395978c15818d921c3f90a16
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-04-18 17:29:15 +0200

    new display

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ let operator = '';
 let display = '';
 let roundedResult = '';
 let actContent = '';
+let displayB='';
+let displayOperator='';
 
 
 function add(a, b) {
@@ -20,7 +22,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b == 0) {
-        alert("You can't divide by 0!");
+        alert("You can't divide by 0!"); // jak zzmeniam operator z / na inny to psuje sie :)
     } else {
         return (a / b);
     }
@@ -39,9 +41,9 @@ function operate(operator, a, b) {
 }
 
 function roundResult(result) {
-    if (result.length > 10 && !Number.isInteger(result)) {
+    if (result.length > 15 && !Number.isInteger(result)) {
         let indexOfComma = result.indexOf('.');
-        return roundedResult = Math.round(result * Math.pow(10, 9 - indexOfComma)) / (Math.pow(10, 9 - indexOfComma));
+        return roundedResult = Math.round(result * Math.pow(10, 14 - indexOfComma)) / (Math.pow(10, 14 - indexOfComma));
     } else {
         return result;
     }
@@ -50,8 +52,12 @@ function roundResult(result) {
 
 document.querySelectorAll('.button').forEach(el => {
     el.addEventListener('click', event => {
-        display = document.getElementById('display');
+        display = document.getElementById('displayForA');
         actContent = display.textContent;
+        displayB=document.getElementById('displayForB');
+        actContentB=displayB.textContent;
+        displayOperator=document.getElementById('displayForOperator');
+        
         let textOfPressedButton = event.target.textContent;
         if (event.target.classList.contains('number')) {
             if (operator == '') {
@@ -59,77 +65,99 @@ document.querySelectorAll('.button').forEach(el => {
                 display.textContent = actContent + textOfPressedButton;
             } else {
                 b += textOfPressedButton;
-                display.textContent = actContent + textOfPressedButton;
+                displayB.textContent = actContentB + textOfPressedButton;
             }
 
         } else if (event.target.classList.contains('operator')) {
             if (operator != '') {
-                display.textContent = roundResult((operate(operator, a, b)).toString()) + textOfPressedButton;
+                display.textContent = roundResult((operate(operator, a, b)).toString());
+                displayOperator.textContent=textOfPressedButton;
+                displayB.textContent='';
                 a = operate(operator, a, b);
                 operator = event.target.id;
                 b = '';
             } else {
                 operator = event.target.id;
-                display.textContent = actContent + textOfPressedButton;
+                displayOperator.textContent = textOfPressedButton;
             }
 
         } else if (event.target.classList.contains('equals')) {
             display.textContent = roundResult((operate(operator, a, b)).toString());
+            displayB.textContent='';
+            displayOperator.textContent='';
             a = operate(operator, a, b);
-            b = '';
+            b = ''; // problem->jesli zrobię *, potem = i xnowu *->zamiast a mam 0 w wyświetlaczu
+            operator='';
         }
     })
 })
 
 document.getElementById('AC').addEventListener('click', function () {
-    display = document.getElementById('display');
+    display = document.getElementById('displayForA');
+    displayB = document.getElementById('displayForB');
+    displayOperator = document.getElementById('displayForOperator')
     operator = '';
     display.textContent = '';
+    displayB.textContent='';
+    displayOperator.textContent='';
     a = '';
     b = '';
 })
 
 document.addEventListener('keydown', event => {
-    //console.log(parseInt(event.key));
-    display = document.getElementById('display');
+    display = document.getElementById('displayForA');
     actContent = display.textContent;
+    displayB = document.getElementById('displayForB');
+    actContentB = displayB.textContent;
+    displayOperator = document.getElementById('displayForOperator');
+
     let textOfPressedButton = event.key;
     if (!isNaN(parseInt(event.key)) || event.key == '.') {
         if (operator) { //operator nie jest pusty
             b = (b + textOfPressedButton);
-            display.textContent = actContent + textOfPressedButton;
+            displayB.textContent = actContentB + textOfPressedButton;
         } else {
             a = (a + textOfPressedButton);
             display.textContent = actContent + textOfPressedButton;
         }
     } else if (textOfPressedButton == '+' || textOfPressedButton == '-' || textOfPressedButton == '*' || textOfPressedButton == '/') {
         if (operator) {
-            display.textContent = roundResult((operate(operator, a, b)).toString()) + textOfPressedButton;
+            display.textContent = roundResult((operate(operator, a, b)).toString());
             a = operate(operator, a, b);
             operator = textOfPressedButton;
+            displayOperator.textContent=textOfPressedButton;
             b = '';
+            displayB.textContent='';
         } else {
             operator = textOfPressedButton;
-            display.textContent = actContent + textOfPressedButton;
+            displayOperator.textContent = textOfPressedButton;
         }
-    } else {
-        display.textContent = 'ERROR';
+    } else if (textOfPressedButton == '=') {
+        display.textContent = roundResult((operate(operator, a, b)).toString());
+        a = operate(operator, a, b);
+        b = '';
+        operator='';
+        displayB.textContent='';
+        displayOperator.textContent='';
     }
 })
 
 
+
 document.getElementById('BS').addEventListener('click', function () {
-    display = document.getElementById('display');
-    if (display.textContent.slice(display.textContent.length - 1, display.textContent.length) == '+' || display.textContent.slice(display.textContent.length - 1, display.textContent.length)=='-' || display.textContent.slice(display.textContent.length - 1, display.textContent.length)=='/' || display.textContent.slice(display.textContent.length - 1, display.textContent.length)=='*'){
-        display.textContent=display.textContent.substring(0, display.textContent.length - 1);
-} else if (b) {
-    b = b.toString().substring(0, b.toString().length - 1);
-    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
-} else if (a) {
-    a = a.toString().substring(0, a.toString().length - 1);
-    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
-}
+    display = document.getElementById('displayForA');
+    if (display.textContent.slice(display.textContent.length - 1, display.textContent.length) == '+' || display.textContent.slice(display.textContent.length - 1, display.textContent.length) == '-' || display.textContent.slice(display.textContent.length - 1, display.textContent.length) == '/' || display.textContent.slice(display.textContent.length - 1, display.textContent.length) == '*') {
+        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+    } else if (b) {
+        b = b.toString().substring(0, b.toString().length - 1);
+        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+    } else if (a) {
+        a = a.toString().substring(0, a.toString().length - 1);
+        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+    }
 })
+
+
 
 
 
